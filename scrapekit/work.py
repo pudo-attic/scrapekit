8c7e2be6fcf3c7b326210c49a916b4b427216cd4
Friedrich Lindenberg <friedrich@pudo.org>
2014-08-09 10:33:02 +0200

    add comments to worker framework

--- scrapekit/work.py
+++ scrapekit/work.py
@@ -20,6 +20,9 @@ from threading import Thread
 
 
 class TaskManager(object):
+    """ The `TaskManager` is a singleton that manages the threads
+    used to parallelize processing and the queue that manages the
+    current set of prepared tasks. """
 
     def __init__(self, threads=10, max_queue=200, daemon=True):
         """
@@ -39,6 +42,7 @@ class TaskManager(object):
         self.queue = None
 
     def _spawn(self):
+        """ Initialize the queue and the threads. """
         self.queue = Queue(maxsize=self.max_queue)
         for i in range(self.num_threads):
             t = Thread(target=self._consume)
@@ -46,6 +50,9 @@ class TaskManager(object):
             t.start()
 
     def _consume(self):
+        """ Main loop for each thread, handles picking a task off the
+        queue, processing it and notifying the queue that it is done.
+        """
         while True:
             try:
                 task, args, kwargs = self.queue.get(True)
@@ -54,26 +61,48 @@ class TaskManager(object):
                 self.queue.task_done()
 
     def put(self, task, args, kwargs):
+        """ Add a new item to the queue. An item is a task and the 
+        arguments needed to call it.
+
+        Do not call this directly, use Task.queue/Task.run instead.
+        """
         if self.queue is None:
             self._spawn()
         self.queue.put((task, args, kwargs))
 
     def wait(self):
+        """ Wait for each item in the queue to be processed. If this
+        is not called, the main thread will end immediately and none
+        of the tasks assigned to the threads would be executed. """
         if self.queue is None:
             return
         self.queue.join()
 
 
 class Task(object):
+    """ A task is a decorator on a function which helps managing the
+    execution of that function in a multi-threaded, queued context.
 
+    After a task has been applied to a function, it can either be used
+    in the normal way (by calling it directly), through a simple queue
+    (using the `queue` method), or in pipeline mode (using `chain`,
+    `pipe` and `run`).
+    """
+
+    # Singleton instance.
+    # TODO: How can we feed in run-time configuration?
     _manager = TaskManager()
 
-    def __init__(self, fn, threads=None, max_queue=None):
+    def __init__(self, fn):
         self.fn = fn
         self._sinks = []
         self._source = None
 
     def __call__(self, *args, **kwargs):
+        """ Execute the wrapped function. This will either call it in
+        normal mode (returning the return value), or notify any
+        pipeline listeners that have been associated with this task.
+        """
         bind = lambda: self.fn(*args, **kwargs)
         # TODO: lots of exception handling etc.
         if len(self._sinks):
@@ -87,15 +116,24 @@ class Task(object):
             return bind()
 
     def queue(self, *args, **kwargs):
-        """ Schedule a task for execution. """
+        """ Schedule a task for execution. The task call (and its
+        arguments) will be placed on the queue and processed
+        asynchronously. """
         self._manager.put(self, args, kwargs)
         return self
 
     def wait(self):
+        """ Wait for task execution in the current queue to be
+        complete (ie. the queue to be empty). If only `queue` is called
+        without `wait`, no processing will occur. """
         self._manager.wait()
         return self
 
     def run(self, *args, **kwargs):
+        """ Queue a first item to execute, then wait for the queue to
+        be empty before returning. This should be the default way of 
+        starting any scraper.
+        """
         if self._source is not None:
             return self._source.run(*args, **kwargs)
         else:
