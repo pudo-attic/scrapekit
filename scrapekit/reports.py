9ce59a6bc9ed63b62d68e2ef89303de6e6d4b3d3
Friedrich Lindenberg <friedrich@pudo.org>
2014-08-13 23:22:01 +0200

    Begin generating run reports.

--- scrapekit/reports.py
+++ scrapekit/reports.py
@@ -39,4 +39,30 @@ def generate_report(scraper):
     runs = sorted(runs.values(), key=lambda r: r.get('start_time'),
                   reverse=True)
     index_file = paginate(scraper, runs, 'index%s.html', 'index.html')
+    for run in runs:
+        generate_run(scraper, run)
     return index_file
+
+
+def generate_run(scraper, run):
+    prefix = os.path.join('runs', run['id'])
+    tasks = {}
+    for item in log_parse(scraper):
+        task_name = item.get('taskName', 'no_task')
+        if task_name not in tasks:
+            tasks[task_name] = {
+                'name': task_name,
+                'logs': defaultdict(int),
+                'tasks': set(),
+                'messages': 0,
+                'link': os.path.join(prefix, '%s.html' % task_name),
+            }
+        tasks[task_name]['logs'][item.get('levelname')] += 1
+        tasks[task_name]['messages'] += 1
+        if item.get('taskId'):
+            tasks[task_name]['tasks'].add(item.get('taskId'))
+
+    tasks = sorted(tasks.values(), key=lambda r: len(r.get('tasks')),
+                  reverse=True)
+    paginate(scraper, tasks, os.path.join(prefix, 'index%s.html'),
+             'run.html')
