8e191d8321230872cbbf568c914aea82e2f66383
Friedrich Lindenberg <friedrich@pudo.org>
2014-08-14 09:58:42 +0200

    let’s call this a version :)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapekit',
-    version='0.0.1',
+    version='0.1.0',
     description="Light-weight tools for web scraping",
     long_description="",
     classifiers=[
